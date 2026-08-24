b7f29b319f3635004729eec965cd043e95b3da10
palakantil <palak.antil.17.2.2006@gmail.com>
2026-08-24 06:18:10 +0530

    index route,ejs setup

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const listing =require("./models/listing.js");
+const listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/Stayora";
 
@@ -18,22 +19,29 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine" ,"ejs");
+app.set("views",path.join(__dirname , "views"));
+
 app.get("/",(req,res) =>{
     res.send("hi");
 });
 
-app.get("/testlisting", async (req,res) =>{
-    let samplelisting = new listing({
-      title: "my new villa",
-      description: "by the beach",
-      price: 2000,
-      location: "Calangute,goa",
-      country: "India",
-    });
-    await samplelisting.save();
-    console.log("sample was saved");
-    res.send("successful testing");
-});
+app.get("/listing", async (req,res) => {
+  const alllistings = await listing.find({});
+    res.render("listings/index.ejs",{alllistings});
+  });
+// app.get("/testlisting", async (req,res) =>{
+//     let samplelisting = new listing({
+//       title: "my new villa",
+//       description: "by the beach",
+//       price: 2000,
+//       location: "Calangute,goa",
+//       country: "India",
+//     });
+//     await samplelisting.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
 
 app.listen(8080, () => {
     console.log("server is listenning to port 8080");
